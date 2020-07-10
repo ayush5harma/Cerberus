1e888755794f2fded95a0c04cf14ab151a3f5ed5
Shéikh Adnan <manofuranium@gmail.com>
2020-07-10 10:53:04 +0530

    Check

--- userbot/modules/whois.py
+++ userbot/modules/whois.py
@@ -17,7 +17,7 @@ from userbot import CMD_HELP, TEMP_DOWNLOAD_DIRECTORY
 from userbot.events import register
 
 
-@register(pattern=".whois(?: |$)(.*)", outgoing=True)
+@register(pattern=".steal(?: |$)(.*)", outgoing=True)
 async def who(event):
 
     await event.edit(
@@ -130,25 +130,25 @@ async def fetch_info(replied_user, event):
     user_bio = "This User has no About" if not user_bio else user_bio
 
     caption = "<b>USER INFO:</b>\n\n"
-    caption += f"👤First Name: {first_name}\n"
-    caption += f"👤Last Name: {last_name}\n"
-    caption += f"👥Username: {username}\n"
-    caption += f"💻Data Centre ID: {dc_id}\n"
-    caption += f"🖼️Number of Profile Pics: {replied_user_profile_photos_count}\n"
-    caption += f"🤖Is Bot: {is_bot}\n"
-    caption += f"❌Is Restricted: {restricted}\n"
-    caption += f"☑️Is Verified by Telegram: {verified}\n"
-    caption += f"ℹ️ID: <code>{user_id}</code>\n\n"
-    caption += f"🔮Bio: \n<code>{user_bio}</code>\n\n"
-    caption += f"⌨️Common Chats with this user: {common_chat}\n"
-    caption += f"🖇️Permanent Link To Profile: "
+    caption += f"First Name: {first_name}\n"
+    caption += f"Last Name: {last_name}\n"
+    caption += f"Username: {username}\n"
+    caption += f"Data Centre ID: {dc_id}\n"
+    caption += f"Number of Profile Pics: {replied_user_profile_photos_count}\n"
+    caption += f"Is Bot: {is_bot}\n"
+    caption += f"Is Restricted: {restricted}\n"
+    caption += f"Is Verified by Telegram: {verified}\n"
+    caption += f"ID: <code>{user_id}</code>\n\n"
+    caption += f"Bio: \n<code>{user_bio}</code>\n\n"
+    caption += f"Common Chats with this user: {common_chat}\n"
+    caption += f"Permanent Link To Profile: "
     caption += f"<a href=\"tg://user?id={user_id}\">{first_name}</a>"
 
     return photo, caption
 
 
 CMD_HELP.update({
-    "whois":
-    ".whois <username> or reply to someones text with .whois\
+    "stealinfo":
+    ".steal <username> or reply to someones text with .steal\
     \nUsage: Gets info of an user."
 })
