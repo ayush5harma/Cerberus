00d1b0ed3129ce14dfcca7f0da2260774c470fb7
Shéikh Adnan <manofuranium@gmail.com>
2020-07-19 20:59:09 +0530

    Check

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -26,7 +26,7 @@ async def gen_chlog(repo, diff):
     ch_log = ''
     d_form = "%d/%m/%y"
     for c in repo.iter_commits(diff):
-        ch_log += f'`{c.summary}` \nby {c.author} \non [{c.committed_datetime.strftime(d_form)}]\n'
+        ch_log += f'{c.summary} \nby {c.author} \non [{c.committed_datetime.strftime(d_form)}]\n'
     return ch_log
 
 
