1900c93d391ce3b4ac64ec69106e89a8454a7395
Shéikh Adnan <manofuranium@gmail.com>
2020-07-23 08:18:14 +0530

    Just checking
    Thanks to @A_FRICKING_GAMER for hyper link module

---
+++ userbot/modules/hyperlink.py
@@ -0,0 +1,23 @@
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# you may not use this file except in compliance with the License.
+
+# By Priyam Kalra
+# Syntax (.hl <link>)
+
+from telethon import events
+from telethon.tl import functions, types
+from userbot import CMD_HELP
+from userbot.events import register
+import asyncio
+
+@register(outgoing=True, (pattern="hl ?(.*)"))
+async def _(event):
+        if event.fwd_from:
+            return
+        input = event.pattern_match.group(1)
+        await event.edit("[ㅤㅤㅤㅤㅤㅤㅤ](" + input + ")")
+CMD_HELP.update({
+"hl":
+	"Use :- .hl <word> <link>"})
