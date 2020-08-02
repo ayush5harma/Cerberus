d0d264831c6af84cbbe7faa3072f2060741e355e
Shéikh Adnan <manofuranium@gmail.com>
2020-08-02 07:24:28 +0530

    Updated fonts config

--- userbot/modules/fonts.py
+++ userbot/modules/fonts.py
@@ -1,4 +1,8 @@
-
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# you may not use this file except in compliance with the License.
+# This module is maked by Project TESLA
 
 from userbot.events import register
 from userbot import CMD_HELP
